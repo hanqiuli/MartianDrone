56cbcdc806f6e7f13a32742f67f6cf5044f730bc
degekeman <126735674+degekeman@users.noreply.github.com>
2024-05-13 17:24:43 +0200

    Added iteration for tiltrotor and wing, broken calculation somewhere however

--- DesignTools/Horizontalflight.py
+++ DesignTools/Horizontalflight.py
@@ -1,8 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 from environment_properties import ENV
+from rotor import Rotor
+
 
 class Horizontal:
-   def __init__(self, config, b, m_init, Sf, M_MO, V_stall, *, Cfc=0.005, LambdaLE=0, e_min=0.7, e_max=0.85, eta_prop=0.5, eta_power=0.6, E_spec=828000, Lf=2, Rf=0.15):
+   def __init__(self, config, b, m_init, Sf, M_MO, V_stall, *, CL_max=1.5, w_max=4.5, update_b=True, Range=20000, Endurance=1800, Cfc=0.0065, LambdaLE=0, e_min=0.7, e_max=0.85, eta_prop=0.5, eta_power=0.6, E_spec=828000, Lf=2, Rf=0.15):
       '''
       Inputs:
          config   [-]   design configuration          string     
@@ -12,7 +16,12 @@ class Horizontal:
          Sf       [m^2] Fuselage wetted area          array-like
          M_MO     [-]   Maximum operating mach number float/int
          V_stall  [m/s] Stall speed                   float/int
-         Cfc      [-]   Skin friction coefficient     array-like
+         CL_max   [-]   Maximum lift coefficient      float/int
+         w_max    [m]   Maximum width of drone        float/int
+         update_b [-]   Update b w.r.t. rotor radius  bool
+         Range    [m]   Range to be sized for         float/int
+         Endurance[s]   Endurance to be sized for     float/int
+         Cfc      [-]   Skin friction coefficient     array-like        https://archive.org/details/aircraftdesignco0000raym_y3s8/page/328/mode/2up?view=theater
          e_min    [-]   oswald efficiency minimum     float/int
          e_max    [-]   oswald efficiency maximum     float/int
          eta_prop [-]   Propeller efficiency          float/int
@@ -26,6 +35,8 @@ class Horizontal:
       self.b         = b
       self.m         = m_init
       self.Sf        = Sf
+      self.CL_max    = CL_max
+      self.w_max     = w_max
       self.Cfc       = Cfc
       self.LambdaLE  = LambdaLE
       self.e_min     = e_min
@@ -36,10 +47,15 @@ class Horizontal:
       self.Lf        = Lf
       self.Rf        = Rf
       self.config    = config
+      self.Range     = Range
+      self.Endurance = Endurance
+      self.update_b  = update_b
+
 
       # Derived variables
       self.V_MO      = M_MO * ENV['a']
       self.eta_total = eta_prop * eta_power
+      self.WS_max    = CL_max * 0.5*ENV['rho']*V_stall**2
 
       if 'tiltrotor' in self.config:
          contingency = 0.2       # extra weight fraction added
@@ -52,20 +68,42 @@ class Horizontal:
 
          hoverTime   = 120       # [s] time spent hovering/converting to horizontal
 
+         # rotor
+         N        = 2               # Number of ROTORS
+         N_blades = 4               # Number of blades per rotor
 
-      elif 'tiltwing' in self.config:
+      elif 'tiltwing2' in self.config:
          contingency = 0.2       # extra weight fraction added
 
          empennageWF = 0.1       # empennage weight fraction of wing mass
          tiltwingWF  = 0.1       # tilting mechanism weight fraction of wing mass
          tiltpropWF  = 0.2       # tilting mechanism weight fraction of propulsion mass
 
-         winginterf  = 0.1       # Wing rotor interference  (FIND PROPER SOURCE)
+         winginterf  = 0         # Wing rotor interference  (FIND PROPER SOURCE)
 
          hoverTime   = 30        # [s] time spent hovering/converting to horizontal
 
+         # rotor
+         N        = 2            # Number of ROTORS
+         N_blades = 4            # Number of blades per rotor
+      
+      elif 'tiltwing4' in self.config:
+         contingency = 0.2       # extra weight fraction added
+
+         empennageWF = 0.1       # empennage weight fraction of wing mass
+         tiltwingWF  = 0.1       # tilting mechanism weight fraction of wing mass
+         tiltpropWF  = 0.2       # tilting mechanism weight fraction of propulsion mass
+
+         winginterf  = 0         # Wing rotor interference  (FIND PROPER SOURCE)
+
+         hoverTime   = 30        # [s] time spent hovering/converting to horizontal
+
+         # rotor
+         N        = 4            # Number of ROTORS
+         N_blades = 4            # Number of blades per rotor
+
       else:
-         raise ValueError('Selected flight configuration is invalid,\npossible configurations are: "tiltwing", "tiltrotor"')
+         raise ValueError('Selected flight configuration is invalid,\npossible configurations are: "tiltwing2", "tiltwing4", or "tiltrotor"')
 
       self.configV = {
          'contingency': contingency,
@@ -75,7 +113,12 @@ class Horizontal:
          'winginterf' : winginterf ,
          'hoverTime'  : hoverTime  ,
       }
-      
+
+      # Rotor class
+      M_tip    = 0.7             # Tip Mach number
+
+      self.rotor = Rotor(M_tip, N, N_blades)
+         
 
    def GetAero(self, S):
       '''
@@ -176,15 +219,17 @@ class Horizontal:
       CL_opt_e = np.maximum(CL_opt_e, CL_min)
       return CL_opt_e
 
-   def P_r_prop(self, CL_opt_r, V_req_r, Cd_r):
+   def P_req_h(self, CL_opt_r, V_req_r, Cd_r, W):
       '''
       Inputs:
-         CL_opt_r [-]   Optimal lift coefficient for optimal range      array-like
-         V_req_r  [m/s] Required true airspeed for optimal range        array-like
-         Cd_r     [-]   Drag coefficient                                array-like
+         CL       [-]   Optimal lift coefficient for                    array-like
+         V_req    [m/s] Required true airspeed                          array-like
+         Cd       [-]   Drag coefficient                                array-like
+         W        [N]   Weight                                          array-like
+      Outputs:
+         P_req    [W]   Required power for horizontal flight            array-like
       '''
 
-      W = ENV['g'] * self.m
       
       return (Cd_r / CL_opt_r * W * V_req_r)
    
@@ -201,7 +246,7 @@ class Horizontal:
       cfV = self.configV
       Ws = self.Getweightsys(S)
 
-      Wnew = (1+cfV['contingency']) * (
+      W = (1+cfV['contingency']) * (
               Ws['wing'] * (1+cfV['tiltwingWF']+cfV['empennageWF']) 
             + Ws['fuselage'] 
             + W_prop * (1+cfV['tiltpropWF'])
@@ -209,7 +254,9 @@ class Horizontal:
             + Ws['const']
       )
 
-      return Wnew
+      self.m = W/ENV['g']
+
+      return W
 
    def Getweightsys(self, S):
       '''
@@ -228,27 +275,29 @@ class Horizontal:
 
       ### Fuselage weight
       Pmax = 2*np.pi*self.Rf                       # [m]  fuselage perimeter
-      W_sys['fuselage'] = 14.86*(self.m*ENV['g']/4.44822162)**0.144*(self.Lf/Pmax)**0.778*(lf/0.3048)**(0.383)
+      W_sys['fuselage'] = 14.86*(self.m*ENV['g']/4.44822162)**0.144*(self.Lf/Pmax)**0.778*(self.Lf/0.3048)**(0.383)
 
       ### Battery weight
       # Range
-      CL_r = self.CLrange(S)
-      CD_r = self.CD(CL_r)
-      W_Bat_r = 20000   *ENV['g']/self.E_spec * CD_r/CL_r * W/self.eta_total
+      self.CL_r = self.CLrange(S)
+      self.CD_r = self.CD(self.CL_r)
+      self.V_r  = self.V(self.CL_r, S)
+      W_Bat_r = self.Range      *ENV['g']/self.E_spec * self.CD_r/self.CL_r * W/self.eta_total
 
       # Endurance
-      CL_e = self.CLend(S)
-      CD_e = self.CD(CL_e)
-      W_Bat_e = 1800    *CD_e/CL_e * W * self.V(CL_e, S) / self.eta_total / self.E_spec * ENV['g']
+      self.CL_e = self.CLend(S)
+      self.CD_e = self.CD(self.CL_e)
+      self.V_e  = self.V(self.CL_e, S)
+      W_Bat_e = self.Endurance  *self.CD_e/self.CL_e * W * self.V_e / self.eta_total / self.E_spec * ENV['g']
 
       W_sys['battery'] = np.maximum(W_Bat_r, W_Bat_e)
 
       ### Wing weight
       Nz = 2.25   #design load factor
       taper = 1.00   #taper ratio
-      AR = self.b**2/S #Aspect ratio
+      AR = self.b**2 / S #Aspect ratio
       t_c_root = 0.1 #thickness/chord ratio root
-      S_csw = 0.15**2*S
+      S_csw = 0.15**2 * S
       sweep_cos = np.cos(self.LambdaLE) #cos sweep=le sweep
       
       W_sys['wing'] = 0.0051 * (Nz**0.557) * S**0.649 * AR**0.5 * t_c_root**(-0.4) * (1+taper)**(-1.0) * S_csw**0.1
@@ -256,10 +305,80 @@ class Horizontal:
       return W_sys
       
    def iterate_step(self):
-      S = np.arange(2, 6, 0.1)
+      ### Rotor step
+      T_req_tot   = (self.m * ENV['g']) / (1-self.configV['winginterf'])
 
+      W_rot       = self.rotor.calc_masses(T_required_tot=T_req_tot, t_flight=self.configV['hoverTime'], E_spec=self.E_spec)
+      P_req_v     = self.rotor.P_total
+      r_rotor     = self.rotor.r_disk
+
+      if self.update_b:
+         self.b = self.w_max - r_rotor
+
+      ### Horizontal step
+      S_low  = 1
+      S_high = 6
+      S_res  = 0.001
+      S = np.arange(S_low, (S_high+S_res), S_res)
 
       self.GetAero(S)
+
+      W = self.Getweight(S, W_rot['prop'], W_rot['battery'])
+
+      ValidStall = (W/S < self.WS_max)
+      MaskedW = W[ValidStall]
+
+      if np.all(~ValidStall):
+         print(W_rot)
+         raise Exception(f"Wing loading of {self.WS_max} could not be met within S of {S_low} and {S_high},\n Wing loadings are:\n\
+         {W/S}")
+
+      i_opt = ValidStall.nonzero()[0][np.argmin(MaskedW)] # converts back from masked index to global index
+      S_opt = S[i_opt]
+      W_opt = W[i_opt]
+
+      
+      if S_opt == S_low or S_opt == S_high:
+         print(f"Optimal S is an endpoint: S = {S_opt}, indicating bad bounds")
+      
+      
+
+      # Streamlines values back to floats (lazily) (also performes half iteration, source of error???)
+      self.m = W_opt/ENV['g']
+      self.GetAero(S_opt)
+      self.Getweight(S_opt, W_rot['prop'], W_rot['battery'])
+      
+      P_req_h_e = self.P_req_h(self.CL_e, self.V_e, self.CD_e, W_opt)
+      P_req_h_r = self.P_req_h(self.CL_r, self.V_r, self.CD_r, W_opt)
+
+      P_req_h   = min(P_req_h_e, P_req_h_r)
+
+      if P_req_h == P_req_v:
+         raise NotImplementedError(f"Horizontal power required, {P_req_h}, is larger than vertical power required, {P_req_v}.\nThis case has not been covered yet")
+
+      return S_opt, W_opt, P_req_h, P_req_v
+
+
+   def iteration(self, IterMax, eps=1e-6):
+      self.S_arr  = np.zeros(IterMax)
+      self.W_arr  = np.zeros(IterMax)
+      self.Ph_arr = np.zeros(IterMax)
+      self.Pv_arr = np.zeros(IterMax)
+
+      for i in range(IterMax):
+         print(i)
+         itVals = self.iterate_step()
+
+         self.S_arr [i] = itVals[0]
+         self.W_arr [i] = itVals[1]
+         self.Ph_arr[i] = itVals[2]
+         self.Pv_arr[i] = itVals[3]
+
+      plt.plot(range(IterMax), [self.S_arr, self.W_arr, self.Ph_arr, self.Pv_arr])
+      plt.show()
+
+
+
 
 if __name__=="__main__":
    b = 4
@@ -268,5 +387,7 @@ if __name__=="__main__":
    Sf = np.pi*2*0.3
    Cfc = 0.005
 
-   hTR = Horizontal('tiltrotor', b, 82.23, Sf, 0.7 , V_stall = 15)
-   hTW = Horizontal('tiltwing', b, 82.23, Sf, 0.7 , V_stall = 15)
+   hTR = Horizontal('tiltrotor', b, 82.23, Sf, 0.7 , V_stall = 100)
+   hTW = Horizontal('tiltwing2', b, 82.23, Sf, 0.7 , V_stall = 110)
+
+   hTR.iteration(10000)
--- DesignTools/rotor.py
+++ DesignTools/rotor.py
@@ -2,24 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from environment_properties import ENV
 
-env = ENV
-
-#Define design parameters
-M_tip = 0.7     # Tip Mach number
-N = 1           # Number of ROTORS
-N_blades = 4    # Number of blades per rotor
-T_W_max = 1.2   # Thrust margin
-T_A_disk = 8.5  # Disk loading [N/m^2]
-
-# Define aerodynamic constants for rotor
-ct_sigma = 0.115        # blade loading coefficient
-cl_mean = 6 * ct_sigma  # mean lift coefficient
-cd_mean = cl_mean/10    # mean drag coefficient
-k_hover = 1.2           # induced/ideal power ratio
-
 
 class Rotor:
-    def __init__(self, M_tip, N, N_blades, T_W_max, T_A_disk, ct_sigma, cl_mean, cd_mean, k_hover):
+    def __init__(self, M_tip, N, N_blades, *, T_W_max=1.2, T_A_disk=8.5, ct_sigma=0.115, cl_mean=0.69, cd_mean=0.069, k_hover=1.2):
         self.M_tip = M_tip
         self.N = N
         self.N_blades = N_blades
@@ -30,40 +15,39 @@ class Rotor:
         self.cd_mean = cd_mean
         self.k_hover = k_hover
 
-    def calculate_V_tip(self, a):
-        self.V_tip = self.M_tip * a
+        self.V_tip = M_tip * ENV['a']
 
-    def required_params_per_rotor(self, T_required, thrust_margin):
-        self.A_disk = (thrust_margin * T_required) / self.T_A_disk
+    def required_params_per_rotor(self, T_required):
+        self.A_disk = (self.T_W_max * T_required) / self.T_A_disk
         self.r_disk = np.sqrt(self.A_disk / np.pi)
 
-    def calculate_A_blade(self, T_required, rho):
-        self.A_blade = T_required / (rho * self.ct_sigma * self.V_tip**2)
+    def calculate_A_blade(self, T_required):
+        self.A_blade = T_required / (ENV['rho'] * self.ct_sigma * self.V_tip**2)
 
-    def calculate_ct_and_solidity(self, T_required, rho):
-        self.ct = T_required / (self.A_disk * rho * self.V_tip**2)
+    def calculate_ct_and_solidity(self, T_required):
+        self.ct = T_required / (self.A_disk * ENV['rho'] * self.V_tip**2)
         self.sigma = self.ct / self.ct_sigma
 
     def calculate_omega(self):
         return self.V_tip / self.r_disk
 
-    def calculate_power_per_rotor(self, T_required, rho):
-        self.P_per_rotor = self.k_hover * T_required * np.sqrt((T_required)/(2*rho*self.A_disk)) + \
-            rho * self.A_blade * self.V_tip**3 * self.cd_mean * (1/8)
+    def calculate_power_per_rotor(self, T_required):
+        self.P_per_rotor = self.k_hover * T_required * np.sqrt((T_required)/(2*ENV['rho']*self.A_disk)) + \
+            ENV['rho'] * self.A_blade * self.V_tip**3 * self.cd_mean * (1/8)
+        return self.P_per_rotor
 
     def calculate_power_total(self):
         '''total power required for all rotors'''
         self.P_total = self.P_per_rotor * self.N
 
-    def calculate_total_energy(self, T_flight):
-        self.E_total = (self.P_total * T_flight) / 3600 # Convert to Wh
-        return self.E_total
-
+    def calculate_total_energy(self, t_flight):
+        E_total = self.P_total * t_flight # [J]
+        return E_total
 
     #  Formulas for masses
 
-    def calculate_battery_mass(self, E_specific):
-        return self.E_total / E_specific
+    def calculate_battery_mass(self, t_flight, E_specific):
+        return self.calculate_total_energy(t_flight) / E_specific
 
     def calculate_motor_mass(self):
         return 0.432/1000 * self.P_total
@@ -88,3 +72,38 @@ class Rotor:
         # calculate TOTAL rotor group mass
         return self.calculate_blade_mass() + self.calculate_hub_mass(T_required) + \
                 self.calculate_shaft_mass() + self.calculate_support_mass()
+
+    def calc_masses(self, T_required_tot, t_flight, E_spec):
+        ### Calculate power and energy
+        T_req_pr = T_required_tot / self.N
+
+        self.required_params_per_rotor(T_req_pr)
+        self.calculate_A_blade(T_req_pr)
+        self.calculate_ct_and_solidity(T_req_pr)
+        self.calculate_omega()
+        self.calculate_power_per_rotor(T_req_pr)
+        self.calculate_power_total()
+        self.calculate_total_energy(t_flight)
+
+        ### Calculate masses
+        W_sys = {}
+
+        W_sys['prop']    = ENV['g'] * (self.calculate_rotor_group_mass(T_req_pr) + self.calculate_motor_mass())
+        W_sys['battery'] = ENV['g'] *  self.calculate_battery_mass(t_flight, E_spec)
+
+        return W_sys
+
+if __name__ == "__main":
+
+    #Define design parameters
+    M_tip = 0.7     # Tip Mach number
+    N = 1           # Number of ROTORS
+    N_blades = 4    # Number of blades per rotor
+    T_W_max = 1.2   # Thrust margin
+    T_A_disk = 8.5  # Disk loading [N/m^2]
+
+    # Define aerodynamic constants for rotor
+    ct_sigma = 0.115        # blade loading coefficient
+    cl_mean = 6 * ct_sigma  # mean lift coefficient
+    cd_mean = cl_mean/10    # mean drag coefficient
+    k_hover = 1.2           # induced/ideal power ratio
