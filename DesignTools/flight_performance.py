6cec4ca0f31822034f15b54509fdaac97d5816ad
Salar <salar38ics@gmail.com>
2024-05-19 20:08:36 +0200

    Update flight_performance.py
    Vectorised performance calculations by modifying profile power factor calculation to use boolean mask

--- DesignTools/flight_performance.py
+++ DesignTools/flight_performance.py
@@ -11,13 +11,14 @@ def calc_induced_ideal_power_ratio(mu, k_hover):
     return k_i
 
 def calc_profile_power_factor(mu):
-    if mu == 0:
-        return 1
-    else:
-        t1 = 0.5 * (1 + 6 * mu**2 + mu**4)
-        t2 = 0.25 * (2 + 5 * mu**2) * np.sqrt(1 + mu**2)
-        t3 = 0.375 * mu**4 * np.log((np.sqrt(1 + mu**2) + 1)/(np.sqrt(1 + mu**2) - 1))
-        return t1 + t2 + t3
+    t1 = 0.5 * (1 + 6 * mu**2 + mu**4)
+    t2 = 0.25 * (2 + 5 * mu**2) * np.sqrt(1 + mu**2)
+    t3 = np.zeros_like(mu)
+    mask = mu != 0
+    mu_mask = mu[mask]
+    t3[mask] = 0.375 * mu_mask**4 * np.log(
+        (np.sqrt(1 + mu_mask**2) + 1) / (np.sqrt(1 + mu_mask**2) - 1))
+    return t1 + t2 + t3
 
 def calc_inflow_ratio(CT):
     la = np.sqrt(CT / 2)
@@ -77,23 +78,12 @@ dims = rho * A_tot * V_tip**3
 
 # region Calculations
 advance_ratios = np.linspace(0.0, 0.5, 100)
-airspeeds = [mu * V_tip for mu in advance_ratios]
-CP_is = []
-CP_os = []
-CP_ps = []
-CPs = []
-powers = []
-for mu in advance_ratios:
-    CP_i = calc_induced_power_coeff(k_hover, mu, CT)
-    CP_o = calc_profile_power_coeff(mu, sigma, cl_cd)
-    CP_p = calc_parasite_power_coeff(mu, m, A_tot)
-    CP = CP_i + CP_o + CP_p
-    power = CP * dims
-    CP_is.append(CP_i)
-    CP_os.append(CP_o)
-    CP_ps.append(CP_p)
-    CPs.append(CP)
-    powers.append(power)
+airspeeds = advance_ratios * V_tip
+CP_is = calc_induced_power_coeff(k_hover, advance_ratios, CT)
+CP_os = calc_profile_power_coeff(advance_ratios, sigma, cl_cd)
+CP_ps = calc_parasite_power_coeff(advance_ratios, m, A_tot)
+CPs = CP_is + CP_os + CP_ps
+powers = CPs * dims
 # endregion
 
 # region Plotting
@@ -128,5 +118,4 @@ plot_curves(advance_ratios, [np.divide(CP_is, CPs),
                                                       'Profile Power Ratio $C_{P_o}/C_P$',
                                                       'Parasite Power Ratio $C_{P_p}/C_P$'],
                                                       'Advance Ratio $\mu$', 'Power Ratio')
-
 # endregion
