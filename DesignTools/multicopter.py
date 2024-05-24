3b6982a652687e2dbb05cfeafc61fed138404024
= <h.licai@student.tudelft.nl>
2024-05-24 09:33:52 +0200

    changed back to original instead of validation HEXACOPTER

--- DesignTools/multicopter.py
+++ DesignTools/multicopter.py
@@ -5,7 +5,7 @@ from rotor import Rotor
 
 #Define design parameters
 M_tip = 0.7     # Tip Mach number
-N = 6          # Number of ROTORS
+N = 1          # Number of ROTORS
 N_blades = 4    # Number of blades per rotor
 T_W_max = 1   # Thrust margin
 T_A_disk = 2.29*ENV['g']  # Disk loading [N/m^2]
@@ -18,7 +18,8 @@ k_hover = 1.2           # induced/ideal power ratio
 
 # Define flight time and battery specific energy, as well as efficiency
 # T_flight = 30*60 # s
-E_bat = 218.5      # Wh/kg
+# E_bat = 218.5      # Wh/kg
+E_bat = 250
 total_eff = 0.7
 
 # Define mass constants (in kg)
