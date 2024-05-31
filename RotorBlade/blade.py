273622ebd2c32503644a5c7ee23e96e946d4c37a
Salar <salar38ics@gmail.com>
2024-05-31 17:29:26 +0200

    Implemented angle of attack, inflow angle, thrust and power calculations

--- RotorBlade/blade.py
+++ RotorBlade/blade.py
@@ -1,8 +1,15 @@
+"""# Rotor Blade
+Provides a class that calculates the performance of a rotor blade in hover.
+    - The blade is defined by its planform geometry and pitch distribution.
+    - The Reynolds number distribution over the rotor blade is calculated.
+    - The thrust coefficient of the rotor blade and the rotor are calculated.
+    - The thrust of the rotor is calculated based on the thrust coefficient and the speed of the rotor tip.
+"""
 import numpy as np
 
 class Blade:
     def __init__(self, radius_rotor: float, num_blades: int, radial_nondim_stations: list[float], chord_nondim_stations: list[float]):
-        """Initializes the planform of the rotor blade.
+        """Initializes the rotor blade.
         Args:
             radius_rotor: The radius of the rotor disk. [m]
             num_blades: The number of blades per rotor disk. [-]
@@ -13,18 +20,71 @@ class Blade:
         self.num_blades = num_blades
         self.radial_nondim = np.linspace(radial_nondim_stations[0], radial_nondim_stations[-1], 10000)
         self.chord_nondim = np.interp(self.radial_nondim, radial_nondim_stations, chord_nondim_stations)
+        self.radial = self.radial_nondim * self.radius_rotor
         self.chord = self.chord_nondim * self.radius_rotor
+
+        self.solidity = self.num_blades * self.chord_nondim / np.pi
+        self.lift_slope = 2 * np.pi
+
         self.leading_edge = 0.25 * self.chord_nondim
         self.trailing_edge = -0.75 * self.chord_nondim
-        self.mean_chord = np.trapz(self.chord_nondim, self.radial_nondim)
-        self.area_blade = np.trapz(self.chord, self.radial_nondim)
+
+        self.mean_chord = np.trapz(self.chord, self.radial_nondim)
+        self.area_blade = np.trapz(self.chord, self.radial)
         self.area_blades = self.area_blade * self.num_blades
         self.area_rotor = np.pi * self.radius_rotor**2
-        self.solidity = self.area_blades / self.area_rotor
+        self.solidity_rotor = self.area_blades / self.area_rotor
         self.aspect_ratio = self.radius_rotor / np.mean(self.chord_nondim)
 
-    def calculate_reynolds(self, gamma_air, gas_constant_air, temp_air, density_air, viscosity_air, mach_tip):
-        """Calculates the Reynolds number as a function of the radial position on the rotor blade.
+    def calculate_pitch(self, pitch_root: float, twist: float, pitch_tip: float, reverse_pitch = True):
+        """Calculates the pitch angle of the rotor blade.
+        Args:
+            pitch_root: The pitch angle at the root of the rotor blade. [deg]
+            twist: The twist of the rotor blade. [deg/m]
+            pitch_tip: The pitch angle at the tip of the rotor blade. [deg]
+            reverse_pitch: True for linear increasing pitch, False for inverse decreasing pitch. [bool]
+        """
+        if reverse_pitch:
+            self.pitch = pitch_tip / self.radial_nondim
+        else:
+            self.pitch = pitch_root + twist * self.radial_nondim
+        self.pitch = np.deg2rad(self.pitch)
+
+    def calculate_inflow(self):
+        """Calculates the nondimensionalised inflow in hover.
+        """
+        self.inflow = self.solidity * self.lift_slope / 16 * (np.sqrt(1 + 32 * self.pitch * self.radial_nondim / (self.solidity * self.lift_slope)) - 1)
+        self.inflow_angle = self.inflow / self.radial_nondim
+
+    def calculate_angle_of_attack(self):
+        """Calculates the angle of attack of the rotor blade.
+        """
+        self.angle_of_attack = self.pitch - self.inflow / self.radial_nondim
+
+    def calculate_thrust_coefficient(self):
+        """Calculates the thrust slope of the rotor blade.
+        """
+        self.thrust_slope = self.solidity * self.lift_slope / 2 * (self.pitch * self.radial_nondim**2 - self.inflow * self.radial_nondim)
+        self.thrust_coefficient = np.zeros_like(self.radial_nondim)
+        for i in range(len(self.radial_nondim)):
+            self.thrust_coefficient[i] = np.trapz(self.thrust_slope[:i], self.radial_nondim[:i])
+        self.thrust_coefficient_blade = np.trapz(self.thrust_slope, self.radial_nondim)
+        self.thrust_coefficient_rotor = self.thrust_coefficient_blade * self.num_blades
+
+    def calculate_power_coefficient(self):
+        """Calculates the power coefficient of the rotor blade.
+        """
+        self.power_induced_coefficient_blade = np.trapz(self.inflow * self.thrust_slope, self.radial_nondim)
+        self.power_induced_coefficient_rotor = self.power_induced_coefficient_blade * self.num_blades
+
+    def calculate_thrust_and_power(self, density_air: float):
+        """Calculates the thrust and power of the rotor blade.
+        """
+        self.thrust = self.thrust_coefficient_rotor * density_air * self.area_rotor * self.speed_tip**2
+        self.power_induced = self.power_induced_coefficient_rotor * density_air * self.area_rotor * self.speed_tip**3
+
+    def calculate_reynolds(self, gamma_air: float, gas_constant_air: float, temp_air: float, density_air: float, viscosity_air: float, mach_tip: float):
+        """Calculates the Reynolds number distribution over the rotor blade.
         Args:
             gamma_air: The specific heat ratio of air. [-]
             gas_constant_air: The gas constant of air. [J/(kg K)]
