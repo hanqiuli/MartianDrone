9c5de2f32b586338a910dd030b97f0d0892142ee
Salar <salar38ics@gmail.com>
2024-05-31 09:38:42 +0200

    Renamed class to Blade and fixed calculations

--- RotorBlade/blade.py
+++ RotorBlade/blade.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-class Planform:
-    def __init__(self, radius_rotor, num_blades, radial_nondim_stations, chord_nondim_stations):
+class Blade:
+    def __init__(self, radius_rotor: float, num_blades: int, radial_nondim_stations: list[float], chord_nondim_stations: list[float]):
         """Initializes the planform of the rotor blade.
         Args:
             radius_rotor: The radius of the rotor disk. [m]
@@ -16,14 +16,23 @@ class Planform:
         self.chord = self.chord_nondim * self.radius_rotor
         self.leading_edge = 0.25 * self.chord_nondim
         self.trailing_edge = -0.75 * self.chord_nondim
+        self.mean_chord = np.trapz(self.chord_nondim, self.radial_nondim)
         self.area_blade = np.trapz(self.chord, self.radial_nondim)
         self.area_blades = self.area_blade * self.num_blades
         self.area_rotor = np.pi * self.radius_rotor**2
         self.solidity = self.area_blades / self.area_rotor
         self.aspect_ratio = self.radius_rotor / np.mean(self.chord_nondim)
 
-    def calculate_Reynolds(self, gamma_air, heat_ratio_air, temp_air, density_air, viscosity_air, Mach_tip):
-        speed_sound = np.sqrt(gamma_air * heat_ratio_air * temp_air)
-        self.speed_tip = Mach_tip * speed_sound
+    def calculate_reynolds(self, gamma_air, gas_constant_air, temp_air, density_air, viscosity_air, mach_tip):
+        """Calculates the Reynolds number as a function of the radial position on the rotor blade.
+        Args:
+            gamma_air: The specific heat ratio of air. [-]
+            gas_constant_air: The gas constant of air. [J/(kg K)]
+            temp_air: The temperature of the air. [K]
+            density_air: The density of the air. [kg/m^3]
+            viscosity_air: The dynamic viscosity of the air. [kg/(m s)]
+            mach_tip: The tip Mach number. [-]
+        """
+        self.speed_tip = mach_tip * speed_sound
         self.speed = self.speed_tip * self.radial_nondim
-        self.Reynolds = density_air * self.speed * self.chord / viscosity_air
+        self.reynolds = density_air * self.speed * self.chord / viscosity_air
