Fixed mean chord calculation, vectorised thrust coefficient integration, added power coefficient as function of radial position.

RotorBlade/blade.py
@@ -6,6 +6,7 @@ Provides a class that calculates the performance of a rotor blade in hover.
     - The thrust of the rotor is calculated based on the thrust coefficient and the speed of the rotor tip.
 """
 import numpy as np
+from scipy import integrate
 
 class Blade:
     def __init__(self, radius_rotor: float, num_blades: int, radial_nondim_stations: list[float], chord_nondim_stations: list[float]):
@@ -29,7 +30,7 @@ class Blade:
         self.leading_edge = 0.25 * self.chord_nondim
         self.trailing_edge = -0.75 * self.chord_nondim
 
-        self.mean_chord = np.trapz(self.chord, self.radial_nondim)
+        self.chord_mean = np.trapz(self.chord, self.radial_nondim)
         self.area_blade = np.trapz(self.chord, self.radial)
         self.area_blades = self.area_blade * self.num_blades
         self.area_rotor = np.pi * self.radius_rotor**2
@@ -65,23 +66,22 @@ class Blade:
         """Calculates the thrust slope of the rotor blade.
         """
         self.thrust_slope = self.solidity * self.lift_slope / 2 * (self.pitch * self.radial_nondim**2 - self.inflow * self.radial_nondim)
-        self.thrust_coefficient = np.zeros_like(self.radial_nondim)
-        for i in range(len(self.radial_nondim)):
-            self.thrust_coefficient[i] = np.trapz(self.thrust_slope[:i], self.radial_nondim[:i])
+        self.thrust_coefficient = integrate.cumtrapz(self.thrust_slope, self.radial_nondim, initial=0)
         self.thrust_coefficient_blade = np.trapz(self.thrust_slope, self.radial_nondim)
         self.thrust_coefficient_rotor = self.thrust_coefficient_blade * self.num_blades
 
     def calculate_power_coefficient(self):
         """Calculates the power coefficient of the rotor blade.
         """
+        self.power_induced_coefficient = integrate.cumtrapz(self.inflow * self.thrust_slope, self.radial_nondim, initial=0)
         self.power_induced_coefficient_blade = np.trapz(self.inflow * self.thrust_slope, self.radial_nondim)
         self.power_induced_coefficient_rotor = self.power_induced_coefficient_blade * self.num_blades
 
     def calculate_thrust_and_power(self, density_air: float):
-        """Calculates the thrust and power of the rotor blade.
+        """Calculates the thrust and power of the rotor disk.
         """
-        self.thrust = self.thrust_coefficient_rotor * density_air * self.area_rotor * self.speed_tip**2
-        self.power_induced = self.power_induced_coefficient_rotor * density_air * self.area_rotor * self.speed_tip**3
+        self.thrust_rotor = self.thrust_coefficient_rotor * density_air * self.area_rotor * self.speed_tip**2
+        self.power_induced_rotor = self.power_induced_coefficient_rotor * density_air * self.area_rotor * self.speed_tip**3
 
     def calculate_reynolds(self, gamma_air: float, gas_constant_air: float, temp_air: float, density_air: float, viscosity_air: float, mach_tip: float):
         """Calculates the Reynolds number distribution over the rotor blade.

RotorBlade/rotor_blade.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     density_air = 0.012718839
     viscosity_air = 1.3854112e-5
     mach_tip = 0.7
-    radius_rotor = 1.00
+    radius_rotor = 1.20
     num_blades = 4
     radial_nondim_stations = [0.08, 0.25, 0.75, 1.00]
     chord_nondim_stations = [0.12, 0.16, 0.14, 0.10]
@@ -21,18 +21,18 @@ if __name__ == '__main__':
     blade.calculate_thrust_coefficient()
     blade.calculate_power_coefficient()
     blade.calculate_thrust_and_power(density_air)
-    # region Results
-    print(f'Mean chord of the blade: {blade.mean_chord} m')
+    # region Prints
+    print(f'Mean chord of the blade: {blade.chord_mean} m')
     print(f'Area of the blade: {blade.area_blade} m^2')
     print(f'Aspect ratio of the blade: {blade.aspect_ratio}')
     print(f'Area of all blades: {blade.area_blades} m^2')
     print(f'Area of the rotor disk: {blade.area_rotor} m^2')
     print(f'Solidity of the rotor disk: {blade.solidity_rotor}')
     print(f'Thrust coefficient of the rotor: {blade.thrust_coefficient_rotor}')
-    print(f'Thrust of the rotor: {blade.thrust_coefficient_rotor * density_air * blade.area_rotor * blade.speed_tip**2} N')
+    print(f'Thrust of the rotor: {blade.thrust_rotor} N')
     print(f'Induced power coefficient of the rotor: {blade.power_induced_coefficient_rotor}')
-    print(f'Induced power of the rotor: {blade.power_induced} W')
-
+    print(f'Induced power of the rotor: {blade.power_induced_rotor} W')
+    # endregion
     if 0:   # Planform geometry
         plt.plot(blade.radial_nondim, blade.leading_edge, label='Leading Edge')
         plt.plot(blade.radial_nondim, blade.trailing_edge, label='Trailing Edge')
@@ -71,7 +71,7 @@ if __name__ == '__main__':
         plt.minorticks_on()
         plt.grid(which='both')
         plt.show()
-    if 1:   # Pitch, inflow angle and angle of attack
+    if 0:   # Pitch, inflow angle and angle of attack
         plt.plot(blade.radial_nondim, np.rad2deg(blade.pitch), label='Pitch')
         plt.plot(blade.radial_nondim, np.rad2deg(blade.inflow_angle), label='Inflow Angle')
         plt.plot(blade.radial_nondim, np.rad2deg(blade.angle_of_attack), label='Angle of Attack')
@@ -83,7 +83,7 @@ if __name__ == '__main__':
         plt.grid(which='both')
         plt.legend()
         plt.show()
-    if 0:   # Inflow
+    if 1:   # Inflow
         plt.plot(blade.radial_nondim, blade.inflow)
         plt.xlabel('$r/R$ [-]')
         plt.ylabel('$\\lambda$ [-]')
@@ -93,14 +93,22 @@ if __name__ == '__main__':
         plt.grid(which='both')
         plt.show()
     if 1:   # Thrust coefficient and slope
-        plt.plot(blade.radial_nondim, blade.thrust_slope, label='Thrust Coefficient Slope')
-        plt.plot(blade.radial_nondim, blade.thrust_coefficient, label='Thrust Coefficient')
+        plt.plot(blade.radial_nondim, blade.thrust_slope, label='$dC_T/d(r/R)$')
+        plt.plot(blade.radial_nondim, blade.thrust_coefficient, label='$C_T$')
         plt.xlabel('$r/R$ [-]')
-        plt.ylabel('$C_T$, $dC_T/dr$ [-]')
+        plt.ylabel('$C_T$, $dC_T/d(r/R)$ [-]')
         plt.xlim(0, 1)
         plt.ylim(bottom=0)
         plt.minorticks_on()
         plt.grid(which='both')
         plt.legend()
         plt.show()
-    # endregion
+    if 1:   # Power coefficient
+        plt.plot(blade.radial_nondim, blade.power_induced_coefficient)
+        plt.xlabel('$r/R$ [-]')
+        plt.ylabel('$C_{P_i}$ [-]')
+        plt.xlim(0, 1)
+        plt.ylim(bottom=0)
+        plt.minorticks_on()
+        plt.grid(which='both')
+        plt.show()